d110944ac55ea0ad4d4cfc24d0141d347e3f3ac3
MislavCaleta <mislav.caleta@gmail.com>
2026-04-24 12:07:02 +0200

    add new resource paths

--- resources.py
+++ resources.py
@@ -1,4 +1,8 @@
 import torch
+import os
 
 DATA_PATH = "./data/WUMI2024a_wildfires_1984_2024_with_subfires.txt"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+OUTPUT_PATH = "./outputs"
+OUTPUT_TABLES_PATH = os.path.join(OUTPUT_PATH, "tables")
+OUTPUT_FIGURES_PATH = os.path.join(OUTPUT_PATH, "figures")
